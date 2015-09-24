a73f35ad9ed66dc2852af533e5824530713d6c63
BleLLL <sinkus.lukas@gmail.com>
2015-09-24 15:38:00 +0200

    Buy from quote

--- templates/quote_lookup.php
+++ templates/quote_lookup.php
@@ -1,6 +1,10 @@
 <p> A share of <?= htmlspecialchars($qt["name"]) ?> (<?= htmlspecialchars($qt["symbol"]) ?>)
     costs <b>$<?= htmlspecialchars($qt["price"]) ?></b></p>
 
+<form action="buy.php" method="post">
+    <input type="hidden" name="quote" value="<?= $qt["symbol"] ?>"/>
+    <button type="submit" class="btn btn-default">Buy</button>
+</form>
 <div>
     <a href="quote.php">get another quote</a>
 </div>
